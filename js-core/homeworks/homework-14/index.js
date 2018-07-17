4c7563ac1fd6547aeb883863bd8fe13ef327b677
Natalia <natkabutenko93@gmail.com>
2018-07-17 12:36:09 +0300

    updated version homework_14

--- js-core/homeworks/homework-14/index.js
+++ js-core/homeworks/homework-14/index.js
@@ -60,64 +60,68 @@ console.log(regExpEndParse(someWebpackModule, regExpSetpoint, regExpRequiredValu
 
 
 //Task3
-window.onload = function() {
-    let app = {
-	 	questions: [
- 			{textQuestions:'Вопрос №1',
-		 	textAnswers: ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3']},
-		 	{textQuestions:'Вопрос №2',
-		 	textAnswers: ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3']},
-		 	{textQuestions:'Вопрос №3',
-		 	textAnswers: ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3']}
-	 	],
-	 	createContainer() {
-	 		let conteiner = document.createElement('article');
-	 		conteiner.appendChild(this.createCaption());
-	 		conteiner.appendChild(this.createQuestions());
-	 		conteiner.appendChild(this.createButton());
-	 		return conteiner;
+let app = {
+ 	questions: [
+		{
+			textQuestions:'Вопрос №1',
+	 		textAnswers: ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3']
 	 	},
-	 	createCaption() {
-	 		let caption = document.createElement('h1');
-	 		caption.textContent = 'Тест по програмированию';
-	 		return caption;
+	 	{
+	 		textQuestions:'Вопрос №2',
+	 		textAnswers: ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3']
 	 	},
-	 	createQuestions() {
-	 		let id = 0;
-	 		let list = document.createElement('ol');
-	 		this.questions.forEach((questAnswer) => {
-	 			let question = document.createElement('li');
-	 			question.textContent = questAnswer.textQuestions;
-	 			let sectionAnswer = document.createElement('section');
-	 			question.appendChild(sectionAnswer);
-	 				
- 				questAnswer.textAnswers.forEach((elem) => {
- 					let idId = id++;
-					let answer = document.createElement('p');
-					let input = document.createElement('input');
-					input.setAttribute('type', 'checkbox');
-					input.setAttribute('id', `check${idId}`); 
-					let label = document.createElement('label');
-					label.setAttribute('for', `check${idId}`); 
-					label.textContent = elem;
-					answer.appendChild(input);
-					answer.appendChild(label);
-					
-					sectionAnswer.appendChild(answer);
-					question.appendChild(sectionAnswer);
-					list.appendChild(question);
- 				});
-	 		});
-	 		return list;
-	 	},
-	 	createButton() {
-	 		let button = document.createElement('button');
-	 		button.textContent = 'Проверить мои результаты';
-	 		return button;
-	 	},
-	 	render() {
-	 		document.body.appendChild(this.createContainer());
+	 	{
+	 		textQuestions:'Вопрос №3',
+	 		textAnswers: ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3']
 	 	}
-	};
-	app.render();  
+ 	],
+ 	createContainer() {
+ 		let container = document.createElement('article');
+ 		container.appendChild(this.createCaption());
+ 		container.appendChild(this.createQuestions());
+ 		container.appendChild(this.createButton());
+ 		return container;
+ 	},
+ 	createCaption() {
+ 		let caption = document.createElement('h1');
+ 		caption.textContent = 'Тест по програмированию';
+ 		return caption;
+ 	},
+ 	createQuestions() {
+ 		let id = 0;
+ 		let list = document.createElement('ol');
+ 		this.questions.forEach((questAnswer) => {
+ 			let question = document.createElement('li');
+ 			question.textContent = questAnswer.textQuestions;
+ 			let sectionAnswer = document.createElement('section');
+ 			question.appendChild(sectionAnswer);
+ 				
+			questAnswer.textAnswers.forEach((elem) => {
+				let idId = id++;
+				let answer = document.createElement('p');
+				let input = document.createElement('input');
+				input.setAttribute('type', 'checkbox');
+				input.setAttribute('id', `check${idId}`); 
+				let label = document.createElement('label');
+				label.setAttribute('for', `check${idId}`); 
+				label.textContent = elem;
+				answer.appendChild(input);
+				answer.appendChild(label);
+				
+				sectionAnswer.appendChild(answer);
+				question.appendChild(sectionAnswer);
+				list.appendChild(question);
+			});
+ 		});
+ 		return list;
+ 	},
+ 	createButton() {
+ 		let button = document.createElement('button');
+ 		button.textContent = 'Проверить мои результаты';
+ 		return button;
+ 	},
+ 	render() {
+ 		document.body.appendChild(this.createContainer());
+ 	}
 };
+app.render();  
